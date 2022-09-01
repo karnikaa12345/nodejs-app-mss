91aa5d470fcfb33cfb208bf6726796f4962c2c6a
karnikaa12345 <96511993+karnikaa12345@users.noreply.github.com>
2022-09-01 16:31:03 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,11 +1,11 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.127.85.206:9980/',
+  serverUrl: 'http://54.208.216.146:9980/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.projectKey':'NodeJs',
 	    'sonar.login': '9d988128860aaa457ff7729bdfdf996f55cce543',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
